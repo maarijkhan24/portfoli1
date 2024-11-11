62b7e4c15482358e1fb3f40c8c6c42c65476ec5f
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-11-11 13:28:37 +0500

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import About from "../components/About";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Nav from "../components/Nav";
-import Skills from "../components/Skills";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
+import Skills from "@/components/Skills";
 
 
 export default function Home() {
